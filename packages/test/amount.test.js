437bf2d93d5f960d290b868abbabb8860456c191
Seungwoo321 <seungwoo321@gmail.com>
2022-06-20 09:28:50 +0900

    test: update amount test

--- packages/test/amount.test.js
+++ packages/test/amount.test.js
@@ -8,23 +8,28 @@ it('get amount', async () => {
     // expect.assertions(2)
     
     const markets = await exchange.loadMarkets()
-    // const symbol = 'ETH/USD:BTC'
-    const symbol = 'BTC/USD:BTC'
-    const capital = 400000
-    const market = markets[symbol]
-    const entryPrice = market.info.prevClosePrice
 
-    const availableMargin = capital * (1 - +market.info.initMargin - +market.info.maintMargin)
-    const amount = symbol === 'BTC/USD:BTC'
-        ? availableMargin * entryPrice / 100000000
-        : availableMargin / market.info.multiplier / entryPrice * market.info.lotSize
-    const formattedAmount = exchange.amountToPrecision(symbol, amount)
-    const formattedPrice = exchange.priceToPrecision(symbol, market.info.prevClosePrice)
-
-    console.log('availableMargin', availableMargin)
-    console.log('market.info.lotSize', market.info.lotSize)
-    console.log('formattedAmount', formattedAmount)
-    console.log('formattedPrice', formattedPrice)
-    expect(formattedAmount % market.precision.amount).toBe(0)
-    expect(formattedPrice * formattedAmount).toBeLessThan(availableMargin)
+    const getMinimumCapital = (markets, symbol, target) => {
+        const market = markets[symbol]
+        const entryPrice = market.info.prevClosePrice
+        let availableMargin = (1 - +market.info.initMargin - +market.info.maintMargin)
+        if (market.maker) {
+            availableMargin += market?.maker
+        }
+        const amount = symbol === 'BTC/USD:BTC'
+            ? availableMargin * entryPrice / 100000000
+            : availableMargin / market.info.multiplier / entryPrice * market.info.lotSize
+        return target / amount
+    }
+    ['BTC/USD:BTC', 'ETH/USD:BTC'].forEach(symbol => {
+        console.log(symbol, getMinimumCapital(markets, symbol, 100))
+    })
+    // const formattedAmount = exchange.amountToPrecision(symbol, amount)
+    // const formattedPrice = exchange.priceToPrecision(symbol, market.info.prevClosePrice)
+    // console.log('availableMargin', availableMargin)
+    // console.log('market.info.lotSize', market.info.lotSize)
+    // console.log('formattedAmount', formattedAmount)
+    // console.log('formattedPrice', formattedPrice)
+    // expect(formattedAmount % market.precision.amount).toBe(0)
+    // expect(formattedPrice * formattedAmount).toBeLessThan(availableMargin)
 })
