d0df02dcc92a9e320b979c2944644b50155451c4
Seungwoo321 <seungwoo321@gmail.com>
2022-06-25 22:19:43 +0900

    chore: update script apollo-server run script

--- packages/cats-apollo-server/index.js
+++ packages/cats-apollo-server/index.js
@@ -37,11 +37,11 @@ const server = new ApolloServer({
 // Start our server if we're not in a test env.
 // if we're in a test env, we'll manually start it in a test
 if (process.env.NODE_ENV !== 'test') {
-    server.listen().then(() => {
+    const port = process.env.PORT || 4000
+    server.listen({ port }).then(({ url }) => {
         console.log(`
-      Server is running!
-      Listening on port 4000
-      Query at https://studio.apollographql.com/dev
+        🚀  Server is ready at ${url}
+        📭  Query at https://studio.apollographql.com/dev
     `)
     })
 }
