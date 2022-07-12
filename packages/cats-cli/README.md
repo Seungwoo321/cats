e42a23a168e6e079798a7eb3d04277c45351981e
Seungwoo321 <seungwoo321@gmail.com>
2022-07-12 22:23:49 +0900

    fix: resolve not working one line command backtest

--- packages/cats-cli/README.md
+++ packages/cats-cli/README.md
@@ -1,1 +1,9 @@
 # @cats/cli
+
+## example
+
+- backtest
+
+```bash
+cats backtest --exchange-id bitmex --exchange-mode production --symbol BTC/USD:BTC --strategy a --timeframe 1h --startDate "2022-03-20 00:00:00" --endDate "2022-07-01 00:00:00" --token cats -c 100000
+```
--- packages/cats-cli/commands/backtest.js
+++ packages/cats-cli/commands/backtest.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer')
 const backtestPrompt = require('../util/prompt/backtest')
 const validationPrompt = require('../util/prompt/validationPrompt')
 const { dataForge, grademark, debug } = require('@cats/shared-utils')
-const gqlService = require('@cats/helper-gql').service
+const gqlService = require('@cats/helper-gql')
 const strategyModule = require('@cats/helper-strategy')
 const { backtest, analyze } = grademark
 const logger = debug('cats:backtest')
@@ -12,15 +12,19 @@ async function backtestOfGrademark (options) {
     try {
         const promptOptions = await inquirer.prompt(backtestPrompt(options))
         const fullOptions = Object.assign({}, options, promptOptions)
-        
         logger(JSON.stringify(fullOptions))
         if (fullOptions.exchangeMode === 'test' && !validationPrompt.enableTestExchange(fullOptions.exchangeId)) {
             throw new Error(`${fullOptions.exchangeId} is not supported test mode`)
         }
 
         if (validationPrompt.supportableExchange(fullOptions.exchangeId)) {
-
-            const candles = await gqlService.getCandles(
+            if (typeof fullOptions.startDate === 'string') {
+                fullOptions.startDate = new Date(fullOptions.startDate)
+            }
+            if (typeof fullOptions.endDate === 'string') {
+                fullOptions.endDate = new Date(fullOptions.endDate)
+            }
+            const candles = await gqlService.service.getCandles(
                 fullOptions.token,
                 fullOptions.exchangeId,
                 fullOptions.exchangeMode,
@@ -34,9 +38,29 @@ async function backtestOfGrademark (options) {
             }
             let inputSeries = new dataForge.DataFrame(candles).setIndex('time')
             const trades = backtest(strategyModule[fullOptions.strategy], inputSeries)
-            const startingCapital = fullOptions.capital
+            const startingCapital = Number(fullOptions.capital)
             const analysis = analyze(startingCapital, trades)
-            console.table(trades)
+            console.table(trades.map(item => {
+                return {
+                    direction: item.direction,
+                    entryTime: item.entryTime,
+                    entryPrice: item.entryPrice,
+                    exitTime: item.exitTime,
+                    exitPrice: item.exitPrice,
+                    profit: item.profit,
+                    profitPct: item.profitPct,
+                    growth: item.growth,
+                    riskPct: item.riskPct,
+                    // riskSeries: item.riskSeries,
+                    // rmultiple: item.rmultiple,
+                    holdingPeriod: item.holdingPeriod,
+                    exitReason: item.exitReason,
+                    stopPrice: item.stopPrice,
+                    // stopPriceSeries: item.stopPriceSeries,
+                    // profitTarget: item.profitTarget
+
+                }
+            }))
             console.log(analysis)
         }
     } catch (error) {
@@ -46,7 +70,6 @@ async function backtestOfGrademark (options) {
 
 module.exports = (...args) => {
     return backtestOfGrademark(...args).catch(err => {
-        console.log()
         console.log(err?.response?.errors || err)
         if (!process.env.CLI_TEST) {
             process.exit(1)
